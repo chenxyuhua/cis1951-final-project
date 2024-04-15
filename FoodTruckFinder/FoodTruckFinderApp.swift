3c6c3f44676179707b04e41e62b9986afbbc614c
chenxyuhua <98442925+chenxyuhua@users.noreply.github.com>
2024-04-14 22:12:54 -0400

    Initial Commit

---
+++ FoodTruckFinder/FoodTruckFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTruckFinderApp.swift
+//  FoodTruckFinder
+//
+//  Created by 陈昱桦 on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodTruckFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
