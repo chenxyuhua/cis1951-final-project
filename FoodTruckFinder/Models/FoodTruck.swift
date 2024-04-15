a4a4aa195fcf0d2c96fbfd2294ffb7e9bdddf1d3
chenxyuhua <98442925+chenxyuhua@users.noreply.github.com>
2024-04-14 22:54:24 -0400

    Add FoodTruck model

--- FoodTruckFinder/Models/FoodTruck.swift
+++ FoodTruckFinder/Models/FoodTruck.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import CoreLocation
+
+struct FoodTruck {
+    let name: String
+    let location: CLLocationCoordinate2D
+    let hours: String
+    let cuisine: String
+    let avgPrice: Double?
+
+    init(name: String, location: CLLocationCoordinate2D, hours: String, cuisine: String, avgPrice: Double? = nil) {
+        self.name = name
+        self.location = location
+        self.hours = hours
+        self.cuisine = cuisine
+        self.avgPrice = avgPrice
+    }
+}
