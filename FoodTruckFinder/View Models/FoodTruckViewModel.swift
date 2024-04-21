f07d8c605f5d6d27fbff871ffb53bb89665b71d4
chenxyuhua <98442925+chenxyuhua@users.noreply.github.com>
2024-04-21 11:55:47 -0400

    Implement persistent storage & fix HomeView dup issue

--- FoodTruckFinder/View Models/FoodTruckViewModel.swift
+++ FoodTruckFinder/View Models/FoodTruckViewModel.swift
@@ -12,43 +12,37 @@ import CoreLocation
 
 class FoodTruckViewModel: ObservableObject {
     @Published var foodTrucks: [FoodTruck] = []
+    
+    init() {
+        loadFoodTrucks()
+    }
 
+    // Adds a new food truck
     func addFoodTruck(_ truck: FoodTruck) {
         foodTrucks.append(truck)
         saveFoodTrucks()
     }
+    
+    // Saves food trucks to persistent storage
+    func saveFoodTrucks() {
+        if let data = try? JSONEncoder().encode(foodTrucks) {
+            UserDefaults.standard.set(data, forKey: "FoodTrucks")
+        }
+    }
 
     // Loads food trucks from persistent storage
     func loadFoodTrucks() {
-        // SAMPLE DATA LOADING FOR TESTING HOME PAGE
-        let sampleTruck1 = FoodTruck(
-            name: "Liam's",
-            location: CLLocation(latitude: 39.9526976, longitude: -75.1990490),
-            hours: "9 AM to 5 PM except Weekends",
-            averagePrice: 10.0,
-            category: "Chinese",
-            isFavorite: true
-        )
-        let sampleTruck2 = FoodTruck(
-            name: "Halal Food Truck",
-            location: CLLocation(latitude: 39.9512526, longitude: -75.1985373),
-            hours: "9 AM to 5 PM",
-            averagePrice: 8.0,
-            category: "Halal",
-            isFavorite: false
-        )
-        foodTrucks.append(sampleTruck1)
-        foodTrucks.append(sampleTruck2)
-    }
-
-    // Saves food trucks to persistent storage
-    func saveFoodTrucks() {
+        if let data = UserDefaults.standard.data(forKey: "FoodTrucks"),
+           let savedFoodTrucks = try? JSONDecoder().decode([FoodTruck].self, from: data) {
+            foodTrucks = savedFoodTrucks
+        }
     }
     
     func toggleFavorite(_ truck: FoodTruck) {
         if let index = foodTrucks.firstIndex(where: { $0.id == truck.id }) {
-                    foodTrucks[index].isFavorite.toggle()
-                    saveFoodTrucks()
-                }
+            foodTrucks[index].isFavorite.toggle()
+            saveFoodTrucks()
         }
+    }
+    
 }
